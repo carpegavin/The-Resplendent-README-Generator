913df49e88cd63e7cf6f5dadebc1ed05db8a0b01
Gavin Phill <carpe.gavin@gmail.com>
2020-12-10 15:37:48 -0600

    all questions including a list of licenses added; framework of README created.

--- index.js
+++ index.js
@@ -1,63 +1,96 @@
 const inquirer = require('inquirer');
 
-// array of questions for user
-const promptUser = () => 
-  inquirer.prompt([
+inquirer.prompt([
     {
       type: 'input',
       name: 'title',
-    message: 'What is the title of your README?',
+      message: 'What is the title of your README?',
     },
     {
       type: 'input',
       name: 'description',
-    message: 'Please describe your README',
+      message: 'Please describe your README',
     },
     {
       type: 'input',
       name: 'installation',
-    message: 'What are the installation instructions?',
+      message: 'What are the installation instructions?',
     },
     {
       type: 'input',
       name: 'usage',
-    message: 'Enter usage information',
+      message: 'Enter usage information',
     },
     {
       type: 'input',
       name: 'guidlines',
-    message: 'What are the contribution guidlines?',
+      message: 'What are the contribution guidlines?',
     },
     {
       type: 'input',
       name: 'test',
-    message: 'What are the testing instructions?',
+      message: 'What are the testing instructions?',
     },
     {
       type: 'list',
       name: 'license',
-    message: 'Please choose a license from the following',
-    choices: [],
+      message: 'Please choose a license from the following',
+      choices: ["Apache 2.0", "MIT", "GPLv3", "Creative Commons", "Simplified BSD"],
     },
     {
       type: 'input',
       name: 'username',
-    message: 'What is your GitHub username?',
+      message: 'What is your GitHub username?',
     },
     {
       type: 'input',
       name: 'email',
-    message: 'What is your e-mail address?',
+      message: 'What is your e-mail address?',
     },
-])
+]);
+
+
+function generateReadMe(answers) {
+    return `
+    
+    # ${answers.title}
+    
+    ## ${answers.description}
+
+    ### ${answers.license}
+
+    ## Table of Contents
+        1. Installation 
+        2. Usage
+        3. Contribution 
+        4. Test 
+
+    ## Installation Instructions
+        ${answers.installation}
+
+    ## Usage information
+        ${answers.usage}
+
+    ## Contribution guidlines
+        ${answers.guidlines}
+
+    ## Test Instructions
+        ${answers.test}
+    
+    
+    ## Questions?
+        ${answers.username}
+        ### Please e-mail me with any questions!
+        ${answers.email}
+    `
+}
 
     
 
 
 
-// function to write README file
-function writeToFile(fileName, data) {
-}
+
+
 
 // function to initialize program
 function init() {
